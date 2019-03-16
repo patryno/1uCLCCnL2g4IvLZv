c260ac9130f59cf23e615c8008e117aef3d163dc
Patryk Ostrowski <patryk93ostrowski@gmail.com>
2019-03-16 20:16:58 +0100

    small refactor

--- src/Controller/Rest/SwapiRestController.php
+++ src/Controller/Rest/SwapiRestController.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: patryk
- * Date: 16.03.19
- * Time: 19:27
- */
 
 namespace App\Controller\Rest;
 
+use Doctrine\ORM\EntityNotFoundException;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use App\Entity\Person;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
@@ -17,14 +12,26 @@ use Symfony\Component\HttpFoundation\Response;
 class SwapiRestController extends AbstractFOSRestController
 {
     /**
-     * Retrieves an Article resource
+     * Retrieves all information about Person
      * @Rest\Get("/articles/{articleId}")
+     * @throws EntityNotFoundException
      */
     public function getArticle(int $articleId): View
     {
-        $product = $this->getDoctrine()
-            ->getRepository(Person::class)->find(1);
-        // In case our GET was a success we need to return a 200 HTTP OK response with the request object
+        $product = $this->getDoctrine()->getRepository(Person::class)->find($articleId);
+        if (!$product) {
+            throw new EntityNotFoundException('Article with id '.$articleId.' does not exist!');
+        }
         return View::create($product, Response::HTTP_OK);
+    }
+
+    /**
+     * Retrieves all Persons
+     * @Rest\Get("/articles")
+     */
+    public function getArticles(): View
+    {
+        $products = $this->getDoctrine()->getRepository(Person::class)->findAll();
+        return View::create($products, Response::HTTP_OK);
     }
 }
